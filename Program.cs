caf70f063765b64c954610dfe9daafc4ff083b76
jswashburn <washbuj4@my.erau.edu>
2021-01-27 23:49:12 -0500

    Add methods to save/load List<Task> objects to xml

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace TaskTracker
 {
     class Program
     {
+        static List<Task> Tasks { get; set; } = new List<Task>();
+        static XmlSerializer serializer = new XmlSerializer(typeof(List<Task>));
+
         static void Main(string[] args)
         {
 
+        }
+
+        static void SaveTasks(List<Task> tasks)
+        {
+            // Serialize Tasks to file stream
+            using Stream stream = File.Open("./tasks.xml", FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
+            serializer.Serialize(stream, tasks);
+        }
+
+        static void LoadTasks()
+        {
+            // Open file stream and deserialize to Tasks
+            using Stream stream = File.OpenRead("./tasks.xml");
+            Tasks = (List<Task>)serializer.Deserialize(stream);
         }
     }
 }
--- Task.cs
+++ Task.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Runtime.Serialization; // Needed to make objects serializable (ISerializable)
 
-namespace TaskTrackerConsoleApp
+namespace TaskTracker
 {
     public class Task : ISerializable
     {
