30e48b4806c140967d8d038cde8843cae735621b
jswashburn <77169258+jswashburn@users.noreply.github.com>
2021-02-01 14:32:50 -0500

    Test

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace TaskTracker
 
         static void Main(string[] args)
         {
-
+            
         }
 
         static void SaveTasks(List<Task> tasks)
@@ -28,7 +28,6 @@ namespace TaskTracker
             // Open file stream and deserialize to Tasks
             using Stream stream = File.OpenRead("./tasks.xml");
             Tasks = (List<Task>)serializer.Deserialize(stream);
-
         }
     }
 }
